d5d475b23d8aef28c6177f37aae768167e552aff
Martin Ben <mebenav88@gmail.com>
2016-02-02 23:09:20 -0500

    print image links

--- extract_image.py
+++ extract_image.py
@@ -1,59 +1,41 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-# import urllib.request
+import time
 import re
 
+url_list = [
+            "/worldcup/archive/brazil2014/index.html",
+            "/worldcup/archive/southafrica2010/index.html",
+            "/worldcup/archive/germany2006/index.html",
+            "/worldcup/archive/koreajapan2002/index.html",
+            "/worldcup/archive/france1998/index.html",
+            "/worldcup/archive/usa1994/index.html",
+            "/worldcup/archive/italy1990/index.html",
+            "/worldcup/archive/mexico1986/index.html",
+            "/worldcup/archive/spain1982/index.html",
+            "/worldcup/archive/argentina1978/index.html",
+            "/worldcup/archive/germany1974/index.html",
+            "/worldcup/archive/mexico1970/index.html",
+            "/worldcup/archive/england1966/index.html",
+            "/worldcup/archive/chile1962/index.html",
+            "/worldcup/archive/sweden1958/index.html",
+            "/worldcup/archive/switzerland1954/index.html",
+            "/worldcup/archive/brazil1950/index.html",
+            "/worldcup/archive/france1938/index.html",
+            "/worldcup/archive/italy1934/index.html",
+            "/worldcup/archive/uruguay1930/index.html"
+]
+
+for item in url_list:
+    time.sleep(3)
+    req = Request("http://www.fifa.com" + item, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()   #read the page
 
 
-html_page = urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals")
+    page_soup = BeautifulSoup(webpage, "html.parser") #parse the site
+    # order_list = page_soup.find_all("div", {"class": "ph-lnd-12 figure i-wrap "}) #extract anchor tags with class link-wrap and href is true
+    order_list = page_soup.find_all("img", {"class": ["i-lnd-12", "i-lnd-stream-desktop"]}) #extract anchor tags with class link-wrap and href is true
+    # import pdb; pdb.set_trace()
 
-soup = BeautifulSoup(html_page, "html.parser")
-myyear = []
-mylist = []
-
-
-
-for rows in soup.findAll('table')[2].findAll('tr'):
-    # print(link.get('class'))
-    # print(rows)
-    mydict = {}
-
-    #for each row in table data country
-    for tabledata in rows.findAll('td'):
-        # print(row)
-        # country = ''.join(tabledata.findAll(text=True))
-        # print(mystring)
-        name = rows.findAll('td')[0].findAll(text=True)
-        mylist.append(name[0])
-        # print(name)
-        # mydict[name]
-        break
-
-
-
-    # print(rows.findAll('td')[0].findAll(text=True))
-
-
-    # for each row in table header - year
-    for tableheader in rows.findAll('th'):
-        # print(row)
-        year = ''.join(tableheader.findAll(text=True))
-        myyear.append(year)
-    # myyear = myyear[0:]
-myyear = myyear[9:]
-# print(myyear)
-# print(mylist)
-mydict = {}
-i = 0
-for entry in myyear:
-    mydict[entry] = mylist[i]
-    i += 1
-
-print(mydict)
-# print(soup.table.tbody.findAll("tr")[1].text)
-
-
-# import pdb; pdb.set_trace()
-
-# table = soup.findAll('table')
-# print(table)
+    for item in order_list:
+        print(item['src'])
